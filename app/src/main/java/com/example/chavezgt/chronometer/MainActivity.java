18529eeaf9c2537c4ca56bd588800f67ceb29f89
Alfredo Chavez <achave19@ucsc.edu>
2015-01-11 03:48:46 -0800

    Cleaned and commented MainActivity.java

--- app/src/main/java/com/example/chavezgt/chronometer/MainActivity.java
+++ app/src/main/java/com/example/chavezgt/chronometer/MainActivity.java
@@ -32,18 +32,22 @@ private boolean secondTime = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create and register the filters for the ScreenOff
         IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         BroadcastReceiver mReceiver = new ScreenOffReceiver();
         registerReceiver(mReceiver, filter);
-        setContentView(R.layout.activity_main);
+
+        //Set the window flags . Full Screen, show when locked, dismiss keyguard don't change
         Window window = this.getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-
+        //Create the chronometer Object
         chronometer = (Chronometer) findViewById(R.id.chronos);
     }
 
@@ -61,7 +65,6 @@ private boolean secondTime = false;
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -81,7 +84,6 @@ private boolean secondTime = false;
         } else {
             // THIS IS WHEN ONRESUME() IS CALLED WHEN THE SCREEN STATE HAS NOT CHANGED
             resumeChron(findViewById(R.id.chronos));
-
         }
     }
 
@@ -91,25 +93,24 @@ private boolean secondTime = false;
         super.onResume();
     }
 
-
-
-    public  void stopChron(View view){
-
-        pausedTime = SystemClock.elapsedRealtime() - chronometer.getBase();
-        chronometer.stop();
-    }
+    //Starts the chronometer
     public void resumeChron(View view){
-
         chronometer.setBase(SystemClock.elapsedRealtime() - pausedTime);
         chronometer.start();
     }
-    public void goHome(View view){
 
+    //Stops the chronometerand stores the value of it
+    public  void stopChron(View view){
+        pausedTime = SystemClock.elapsedRealtime() - chronometer.getBase();
+        chronometer.stop();
+    }
+
+    //Assign to the Slider. It will send it to the main screen
+    public void goHome(View view){
         Intent homeIntent= new Intent(Intent.ACTION_MAIN);
         homeIntent.addCategory(Intent.CATEGORY_HOME);
         homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(homeIntent);
-
     }
 
 }
