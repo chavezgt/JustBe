aff8e14d04257a28248a0c4d31e43164b876f1bb
Alfredo Chavez <achave19@ucsc.edu>
2015-01-11 03:58:07 -0800

    Cleaned and commented ScreenOffReceiver, changed name of screenOn to screenOff

--- app/src/main/java/com/example/chavezgt/chronometer/ReadScreenService.java
+++ app/src/main/java/com/example/chavezgt/chronometer/ReadScreenService.java
@@ -16,78 +16,53 @@ import android.view.Display;
 public class ReadScreenService extends Service {
     public final static String EXTRA_MESSAGE = "com.example.chavezgt.chronometer..MESSAGE";
 
+    //Default values
     private Looper mServiceLooper;
     /** indicates how to behave if the service is killed */
     int mStartMode;
-    /** interface for clients that bind */
-    IBinder mBinder = new LocalBinder();
     /** indicates whether onRebind should be used */
     boolean mAllowRebind;
-
-    public class LocalBinder extends Binder {
-      //  ReadScreenService  getService(){
-
-     //   }
-    }
+    BroadcastReceiver mReceiver = new ScreenOffReceiver();
 
 
-     @Override
+
+    @Override
      public void onCreate(){
          super.onCreate();
-         //REGISTER A HANDLER
+         //Register filter and receiver for Screen OffReceiver
          IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
          filter.addAction(Intent.ACTION_SCREEN_OFF);
-         BroadcastReceiver mReceiver = new ScreenOffReceiver();
          registerReceiver(mReceiver, filter);
      }
 
-    /** The service is starting, due to a call to startService() */
+    //This executes each time the service is called by ScreenOffReceiver
     @Override
     public int  onStartCommand(Intent intent, int flags, int startId){
-        System.out.println("ME LA PELAS");
-        //Deprecated Mode
-       /* if (ScreenOffReceiver.wasScreenOn) {
-            // THIS IS THE CASE WHEN ONPAUSE() IS CALLED BY THE SYSTEM DUE TO A SCREEN STATE CHANGE
-            System.out.println("SCREEN TURNED OFF");
-        } else {
-        }
-         if (!ScreenOffReceiver.wasScreenOn) {
-            // THIS IS WHEN ONRESUME() IS CALLED DUE TO A SCREEN STATE CHANGE
-            System.out.println("SCREEN TURNED ON");
-           // resumeChron(findViewById(R.id.chronos));
 
-
-        } else {
-            // THIS IS WHEN ONRESUME() IS CALLED WHEN THE SCREEN STATE HAS NOT CHANGED
-        } */
-        boolean screenOn = ScreenOffReceiver.screenOff;
-        if(screenOn){
-            System.out.println("Apagada");
+        //Checks if there has been a broadcast for a change in the state of the screen
+        boolean screenOff = ScreenOffReceiver.screenOff;
+        //if On: call startActivity to execute it. ACTIVITY NEEDS TO BE SET TO LAUNCHMODE SINGLE TASK IN MANIFEST
+        if(screenOff){
+            //Create intent, send necessary flags and start
             Intent i = new Intent(this, MainActivity.class);
             i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-            i.putExtra("com.example.chavezgt.chronometer.MESSAGE", true);
-            //helloo
             this.startActivity(i);
-
-        }else {
-
         }
 
         return START_STICKY;
 
     }
 
-    public void receiveMsg() {
-
-    }
         @Override
     public IBinder onBind(Intent intent){
         //return null
         return null;
 
     }
+
+    //Needs to unregister the receiver
     @Override
     public void onDestroy(){
-
+        unregisterReceiver(mReceiver);
     }
 }
